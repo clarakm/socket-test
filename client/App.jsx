b78640db669b7ae02793156037d8f5bdff943233
Clara <clarayhkim96@gmail.com>
2020-02-25 23:38:13 -0800

    day 1

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
+import Weather from "./components/Weather.jsx";
 
 const App = props => {
   return (
     <div>
       <h2>My App</h2>
+      <Weather />
     </div>
   );
 };
